3578c3ec5411e28fa0ad9c06fb7a1f18f15032de
Lyakh-Sergey <me@ljah.ru>
2025-05-10 22:32:31 +0300

    Set is all done.

--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -2,6 +2,8 @@ package org.skypro.skyshop.product;
 
 import org.skypro.skyshop.Searchable;
 
+import java.util.Objects;
+
 public abstract class Product implements Searchable {
     private final String name;
 
@@ -38,5 +40,18 @@ public abstract class Product implements Searchable {
     @Override
     public String getSearchableName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
     }
 }
